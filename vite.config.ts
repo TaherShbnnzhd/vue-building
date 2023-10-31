7d6fb16df6cc6f89e9f505d9fa4cc4fa0452bf81
TaherShbnnzhd <myshatelnet@gmail.com>
2023-10-31 08:45:51 +0330

    [20231031] | [dONE] | Add set up global SCSS variables;

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assets/scss/global.scss";`
+      }
     }
   }
 })
