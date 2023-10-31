af675aab461d0e285c6a6ea909e2eb286f829b26
TaherShbnnzhd <myshatelnet@gmail.com>
2023-10-31 15:41:14 +0330

    [20231031] | [dONE] | Add path config;

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,10 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@core': fileURLToPath(new URL('./src/@core', import.meta.url)),
+      '@layouts': fileURLToPath(new URL('./src/@layouts', import.meta.url)),
+      '@shared': fileURLToPath(new URL('./src/@shared', import.meta.url)),
     }
   },
   css: {
